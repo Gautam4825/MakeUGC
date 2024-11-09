c4abdde9846dd79ffd1654f11f7825af8df31fec
Gautam Dandgawhal <gautamdandgawhal@gmail.com>
2024-11-09 22:18:10 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  base: './', // Make sure it's relative if needed
+  build: {
+    rollupOptions: {
+      external: [],
+    }
+  },
 });
